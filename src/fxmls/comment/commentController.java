2a4d924212fd37f6fec016db7c1c0dfd5316f071
Elias <erodinge@htl-steyr.ac.at>
2020-11-18 10:39:49 +0100

    Test-Push

--- src/fxmls/comment/commentController.java
+++ src/fxmls/comment/commentController.java
@@ -1,12 +1,20 @@
 package fxmls.comment;
 
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 public class commentController {
+    public Stage backToStartStage;
+    public Button backToStartButton;
+
     public void confirmClicked(ActionEvent actionEvent) {
     }
 
     public void backToStartClicked(ActionEvent actionEvent) {
+        backToStartStage = (Stage) backToStartButton.getScene().getWindow();
+        backToStartStage.close();
     }
 
     public void deleteClicked(ActionEvent actionEvent) {
