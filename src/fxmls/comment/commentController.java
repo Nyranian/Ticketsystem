30d86e8e212e0eaf2490a473416ae71aea2b4790
Elias <erodinge@htl-steyr.ac.at>
2020-11-18 10:45:17 +0100

    Test-Push

--- src/fxmls/comment/commentController.java
+++ src/fxmls/comment/commentController.java
@@ -3,11 +3,17 @@ package fxmls.comment;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class commentController {
     public Stage backToStartStage;
     public Button backToStartButton;
+    public TextField titleField;
+    public TextArea textField;
+    public DatePicker dateField;
 
     public void confirmClicked(ActionEvent actionEvent) {
     }
