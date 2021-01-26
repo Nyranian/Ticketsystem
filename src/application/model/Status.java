5657782698d4a8c27ad9d76fb96a025025eb59ab
Elias <erodinge@htl-steyr.ac.at>
2021-01-26 10:09:10 +0100

    Status Constructor eingefügt

--- src/application/model/Status.java
+++ src/application/model/Status.java
@@ -11,50 +11,64 @@ public class Status {
     public String statusName;
     public int statusID;
 
+    public Status(int priorityID) {
+        if (priorityID > 0) {
+            this.statusName = Priority.getById(priorityID).priorityName;
+            this.statusID = priorityID;
+        }
+    }
+
+    public Status() {
+        this.statusName = "";
+        this.statusID = 0;
+    }
+
     @Override
-    public String toString(){
+    public String toString() {
         return statusName;
     }
 
-    public  String newCSVLine(){
+    public String newCSVLine() {
         return statusID + ";" + statusName + "\n";
     }
 
-    public static Status getById(int id){
-        Status obj = null;
-        try {
-            Connection connection = AccessDB.getConnection();
-            Statement statement = null;
-            statement = connection.createStatement();
-            ResultSet result = statement.executeQuery("SELECT  * FROM stati WHERE status_id = " +id);
+    public static Status getById(int id) {
+        if (id > 0) {
+            Status obj = null;
+            try {
+                Connection connection = AccessDB.getConnection();
+                Statement statement = null;
+                statement = connection.createStatement();
+                ResultSet result = statement.executeQuery("SELECT  * FROM stati WHERE status_id = " + id);
 
-            if(result.next()){
-                obj = new Status();
-                obj.statusID = result.getInt("status_id");
-                obj.statusName = result.getString("name");
+                if (result.next()) {
+                    obj = new Status();
+                    obj.statusID = result.getInt("status_id");
+                    obj.statusName = result.getString("name");
+                }
+            } catch (SQLException throwables) {
+                throwables.printStackTrace();
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
 
-        return obj;
+            return obj;
+        }
+        return new Status();
     }
 
 
-
-    public void delete(){
-        try{
+    public void delete() {
+        try {
             Connection connection = AccessDB.getConnection();
 
             Statement statement = null;
             statement = connection.createStatement();
             statement.executeUpdate("DELETE FROM stati WHERE status_id = " + statusID);
-        }catch (SQLException throwables){
+        } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
     }
 
-    public void update(){
+    public void update() {
         try {
             Connection connection = AccessDB.getConnection();
 
@@ -71,16 +85,16 @@ public class Status {
 
     }
 
-    public static ObservableList<Status> loadStatusList(){
+    public static ObservableList<Status> loadStatusList() {
         ObservableList<Status> list = FXCollections.observableArrayList();
 
         try {
             Connection connection = AccessDB.getConnection();
             Statement statement = connection.createStatement();
             ResultSet result = statement.executeQuery("SELECT  * FROM stati");
-            while (result.next()){
+            while (result.next()) {
                 Status s = new Status();
-                s.statusName= result.getString("name");
+                s.statusName = result.getString("name");
                 s.statusID = result.getInt("status_id");
                 list.add(s);
             }
@@ -91,7 +105,7 @@ public class Status {
         return list;
     }
 
-    public static void printToFile(ObservableList<Status> statusList){
+    public static void printToFile(ObservableList<Status> statusList) {
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter("stati.csv"));
             for (Status s : statusList) {
